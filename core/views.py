8291afa9f9dd088a672039758bbe629c2508f341
Specialistik <test04@list.ru>
2016-11-11 02:04:21 +0000

    base models

--- core/views.py
+++ core/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from models import Category, Product
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html', {'categories': Category.objects.filter(pid__isnull=True)})
+    
+
+def category(request, id, url):
+    if Category.objects.get(pk=id).has_children():
+        return category_list(request, id, url)
+    else:
+        return category_items(request, id, url)
+
+
+def category_list(request, id, url):
+    return render(request, 'category_list.html', {'categories': Category.objects.filter(category=id)})
+    
+    
+def category_items(request, id, url):
+    return render(request, 'category_items.html', {'categories': Product.objects.filter(category=id)})
+
+
+def article(request, id, url):
+    return render(request, 'article.html', {'categories': Category.objects.filter(pid__isnull=True)})
+
+
+def product(request, id, url):
+    return render(request, 'product.html')
